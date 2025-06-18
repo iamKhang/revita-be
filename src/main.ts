a1db271aa17c1cff445ceea8972b6299b7415475
Lê Hoàng Khang <iamhoangkhang@icloud.com>
2025-06-18 11:54:46 +0700

    Cấu hình Swagger cho dự án

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Cấu hình Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API documentation for the Medical Appointment Booking and Patient Information Management System, enabling seamless integration across healthcare application components.')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Đặt endpoint cho Swagger UI tại /api
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
